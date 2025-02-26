6631fef7563e618468ffae7f402ac9627bdb8a1e
Vishal Singh <singh287686@gmail.com>
2025-02-26 10:37:39 +0530

    Implement client-side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Button, Callout, TextField } from '@radix-ui/themes'
+import { Button, Callout, Text, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import "easymde/dist/easymde.min.css";
@@ -8,11 +8,12 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { AiFillInfoCircle, } from 'react-icons/ai';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from 'zod'
 
-interface IssueForm {
-    title: string;
-    description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
 
 const NewIssuePage = () => {
 
@@ -20,7 +21,9 @@ const NewIssuePage = () => {
 
     const [error, setError]  = useState('')
 
-     const {register, control, handleSubmit} = useForm<IssueForm>()
+     const {register, control, handleSubmit, formState: { errors }} = useForm<IssueForm>({
+        resolver: zodResolver(createIssueSchema)
+     })
 
      
 
@@ -45,13 +48,21 @@ const NewIssuePage = () => {
             router.push("/issues");
           } catch (error) {
             setError("An unexpected error occured.");
+            console.log(error)
           }
         })}
       >
+        <div className='flex flex-col '>
         <TextField.Root
           placeholder="Search the docs…"
           {...register("title")}
         ></TextField.Root>
+        {errors.title && <Text color='red' as='p'
+        className='-mt-0'
+        >{errors.title.message}</Text>}
+        </div>
+
+        <div className='flex flex-col '>
         <Controller
           name="description"
           control={control}
@@ -59,8 +70,11 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-
-        <Button type="submit" className='cursor-pointer  hover:violet-800'>Submit New Issue</Button>
+        {errors.description && <Text color='red' as='p'
+        className='-mt-8'
+        >{errors.description.message}</Text>}
+        </div>
+        <Button type="submit" className='cursor-pointer  hover:violet-800 space-y-4'>Submit New Issue</Button>
       </form>
     </div>
   );
